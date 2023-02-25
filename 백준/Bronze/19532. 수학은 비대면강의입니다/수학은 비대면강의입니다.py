851e8d8d475ead45a9ebc5413d98410dc6c6935e
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-02-25 15:26:30 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+import sys; input = lambda: sys.stdin.readline().rstrip()
+
+a, b, c, d, e, f = map(int, input().split())
+for x in range(-999, 1000):
+    flag = False
+    for y in range(-999, 1000):
+        if (a * x) + (b * y) == c and (d * x) + (e * y) == f:
+            flag = True
+            print(x, y)
+            break
+    if flag:
+        break
