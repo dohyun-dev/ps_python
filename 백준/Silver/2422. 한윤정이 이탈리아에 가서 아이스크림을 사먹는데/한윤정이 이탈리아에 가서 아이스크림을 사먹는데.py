84a6f7efff1b0d45b44aed8147d0e771dd3a2aad
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-03-27 21:11:10 +0900

    [Silver V] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 176 ms, Memory: 115540 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -1,19 +1,17 @@
 N, M = map(int, input().split())
-no_combi = {i: set() for i in range(1, N+1)}
+ice_cream = [[True] * (N+1) for _ in range(N+1)]
 answer = 0
 
-for _ in range(M):
+for i in range(M):
     a, b = map(int, input().split())
-    no_combi[a].add(b)
-    no_combi[b].add(a)
+    ice_cream[a][b], ice_cream[b][a] = False, False
 
 for i in range(1, N+1):
     for j in range(i+1, N+1):
-        if j in no_combi[i]:
+        if not ice_cream[i][j]:
             continue
         for k in range(j+1, N+1):
-            if k in no_combi[j] or k in no_combi[i]:
+            if not ice_cream[i][k] or not ice_cream[j][k]:
                 continue
             answer += 1
-            
 print(answer)
