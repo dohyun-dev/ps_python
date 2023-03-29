3ab85022389f6da6d1b0a45a7b038571d6b9d990
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-03-29 16:30:01 +0900

    [Silver III] Title: 전공책, Time: 196 ms, Memory: 116632 KB -BaekjoonHub

--- 백준/Silver/16508. 전공책/전공책.py
+++ 백준/Silver/16508. 전공책/전공책.py
@@ -1,31 +1,21 @@
 from collections import Counter
 
-def calc_price(result):
-    counter = Counter([c for i in result for c in books[i][1]])
-    total = sum(books[i][0] for i in result)
-    for t in T:
-        if t in counter:
-            counter[t] -= 1
-            if counter[t] == 0:
-                counter.pop(t)
-        else:
-            break
-    else:
-        return total
-    return 0
-
-def dfs(l=0, result=[]):
+def dfs(l=0, total_price=0, word_counter=Counter()):
     global answer
-    if l == N:
-        total = calc_price(result)
-        if total != 0:
-            answer = min(answer, total)
+    if answer < total_price:
         return
-    dfs(l+1, result+[l])
-    dfs(l+1, result)
+    if l == N:
+        if len((t_counter - word_counter).keys()) == 0:
+            answer = min(answer, total_price)
+        return
+    price, title = books[l]
+    current_counter = Counter(title)
+    dfs(l+1, total_price+price, word_counter+current_counter)
+    dfs(l+1, total_price, word_counter)
 
 T, N = input(), int(input())
 books = [*map(lambda x: (int(x[0]), x[1]), [input().split() for _ in range(N)])]
+t_counter = Counter(T)
 answer = float("inf")
 dfs()
 print(-1 if answer == float("inf") else answer)
