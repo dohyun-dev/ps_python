9da8f7e1f59aae1b294895f47fd0ceb19d866073
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-04-11 20:49:27 +0900

    [Silver III] Title: N과 M (7), Time: 608 ms, Memory: 118172 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,14 @@
+import sys; input = lambda : sys.stdin.readline().rstrip()
+
+def DFS(l=0, result=[]):
+    if l == M:
+        print(" ".join(result), end="\n")
+        return
+    for i in range(N):
+        result.append(nums[i])
+        DFS(l+1, result)
+        result.pop()
+        
+N, M = map(int, input().split())
+nums = sorted(input().split(), key=lambda x: int(x))
+DFS()
