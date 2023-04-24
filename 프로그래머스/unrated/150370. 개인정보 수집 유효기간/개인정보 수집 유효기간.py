0f5bba989765e7c72db36ed219c408b4f36c38bc
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-04-24 14:33:33 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -2,22 +2,18 @@
 # 각 정책에 따라 day +
 # 현재 날짜와 비교해 더 크다면 폐기
 
-def calc_day(year, month, day):
-    return year * 12 * 28 + (month - 1) * 28 + day
+def calc_day(date):
+    date = list(map(int, date.split(".")))
+    return date[0] * 12 * 28 + date[1] * 28 + date[2]
 
 def solution(today, terms, privacies):
-    today = list(map(int, today.split(".")))
-    today = calc_day(today[0], today[1], today[2])
-    terms_dict = {}
+    today = calc_day(today)
+    terms_dict = {t[0]: int(t[2:]) * 28 for t in terms}
     answer = []
-    
-    for term in terms:
-        name, period = term.split()
-        terms_dict[name] = int(period) * 28
     
     for idx, v in enumerate(privacies, 1):
         date, terms_name = v.split()
-        date = list(map(int, date.split(".")))
-        if today >= calc_day(date[0], date[1], date[2]) + terms_dict[terms_name]:
+        date = calc_day(date)
+        if today >= date + terms_dict[terms_name]:
             answer.append(idx)
     return answer
