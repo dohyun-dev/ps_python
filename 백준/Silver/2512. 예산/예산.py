a7538b5c7aaa78f8c8fce061343b29914eb591ea
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-04-28 15:06:56 +0900

    [Silver III] Title: 예산, Time: 152 ms, Memory: 115688 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,13 @@
+N, arr = int(input()), list(map(int, input().split()))
+limit = int(input())
+left, right = 0, max(arr)
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    total = sum(min(mid, num) for num in arr)
+    if total <= limit:
+        left = mid + 1
+    else:
+        right = mid - 1
+print(right)
