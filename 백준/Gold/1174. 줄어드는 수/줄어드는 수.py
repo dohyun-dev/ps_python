52b70ceea014d9e0606d141b2b9dbfcea923e60b
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-05-31 21:40:52 +0900

    [Gold V] Title: 줄어드는 수, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -1,13 +1,16 @@
-def dfs(result):
-    if len(answer) == 1000000:
+def dfs(result=""):
+
+    if result != "":
+        answer.append(int(result))
+
+    if len(answer) == 10 ** 6:
         return
-    answer.append(int(result))
-    for i in range(int(result[-1])):
+
+    for i in range(int(result[-1]) - 1 if result else 9, -1, -1):
         dfs(result + str(i))
 
 N = int(input())
 answer = []
-for i in range(10):
-    dfs(str(i))
-answer.sort()
-print(answer[N-1] if len(answer) >= N else -1)
+dfs()
+answer.sort(key=lambda x: int(x))
+print(answer[N - 1] if len(answer) >= N else -1)
