5d29df27c726e35ab25b38a308683440e6e69b68
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-05-31 21:47:46 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 122820 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,9 @@
-import sys; input = lambda : sys.stdin.readline().rstrip()
-
-n = int(input())
-dp = [0] * (n + 1)
-dp[1] = 0
-
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
+N = int(input())
+dp = [0] * (N + 1)
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i // 2] + 1)
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i // 3] + 1)
-        
-print(dp[n])
+print(dp[-1])
