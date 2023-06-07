64b51b1fc43a66c43d0ec7f4ad9ea07f86fe595b
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-06-07 19:30:15 +0900

    [Gold V] Title: 선발 명단, Time: 200 ms, Memory: 116512 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.py
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -1,19 +1,20 @@
 def dfs(l=0, total=0):
     global answer
+
     if l == 11:
         answer = max(answer, total)
         return
+    
     for i in range(11):
-        if not positions[l][i] or check[i]:
+        if not board[l][i] or check[i]:
             continue
         check[i] = True
-        dfs(l+1, total + positions[l][i])
+        dfs(l+1, total + board[l][i])
         check[i] = False
 
-
 for t in range(int(input())):
-    answer = 0
-    positions = [list(map(int, input().split())) for _ in range(11)]
+    board = [list(map(int, input().split())) for _ in range(11)]
     check = [False] * 11
+    answer = 0
     dfs()
     print(answer)
