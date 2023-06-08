150c696e73efa6c071086a12fecad2ecf5cc76e8
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-06-08 16:30:01 +0900

    [Gold IV] Title: 무기 공학, Time: 480 ms, Memory: 124348 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.py
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -1,21 +1,29 @@
-dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-def dfs(cnt=0, total=0):
+def dfs(l=0, total=0):
     global answer
-    x, y = cnt // M, cnt % M
-    if x == N:
+
+    if l == N * M:
         answer = max(answer, total)
         return
-    else:
-        if not check[x][y]:
-            for i in range(4):
-                nx1, ny1, nx2, ny2 = dir[i][0] + x, dir[i][1] + y, dir[(i + 1) % 4][0] + x, dir[(i + 1) % 4][1] + y
-                if 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M and not check[nx1][ny1] and not check[nx2][ny2]:
-                    tmp = board[x][y] * 2 + board[nx1][ny1] + board[nx2][ny2]
-                    check[x][y] = check[nx1][ny1] = check[nx2][ny2] = True
-                    dfs(cnt + 1, total + tmp)
-                    check[nx1][ny1] = check[nx2][ny2] = check[x][y] = False
-        dfs(cnt + 1, total)
+
+    x, y = l // M, l % M
+
+    dfs(l + 1, total)
+
+    if check[x][y]:
+        return
+
+    check[x][y] = True
+    for nx1, ny1, nx2, ny2 in [(x-1, y, x, y+1), (x, y+1, x+1, y), (x+1, y, x, y-1), (x, y-1, x-1, y)]:
+        if 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M:
+
+            if check[nx1][ny1] or check[nx2][ny2]:
+                continue
+
+            check[nx1][ny1], check[nx2][ny2] = True, True
+            dfs(l+1, total + (board[x][y] * 2 + board[nx1][ny1] + board[nx2][ny2]))
+            check[nx1][ny1], check[nx2][ny2] = False, False
+    check[x][y] = False
+
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
