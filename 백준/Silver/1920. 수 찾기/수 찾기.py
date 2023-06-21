7b35b5f60067c9a96cc5524e2be1624f65139f1e
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-06-22 01:09:35 +0900

    [Silver IV] Title: 수 찾기, Time: 268 ms, Memory: 141236 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,19 @@
-from bisect import bisect_left
-import sys; input = lambda: sys.stdin.readline().rstrip()
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
 
-N, arr1 = int(input()), list(sorted(map(int, input().split())))
-M, arr2 = int(input()), list(map(int, input().split()))
-for num in arr2:
-    if 0 <= (x:= bisect_left(arr1, num)) < len(arr1) and arr1[x] == num:
-        print(1)
-    else:
-        print(0)
+    while left <= right:
+        mid = (left + right) // 2
+        if target < arr[mid]:
+            right = mid - 1
+        elif target > arr[mid]:
+            left = mid + 1
+        else:
+            return 1
+    return 0
+
+N = int(input())
+nums = sorted(list(map(int, input().split())))
+M = int(input())
+
+for num in map(int, input().split()):
+    print(binary_search(nums, num))
