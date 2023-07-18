4f5bdef9f322e2a00ac8a4d4903440340caf5a48
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-07-18 22:34:05 +0900

    [level 1] Title: 폰켓몬, Time: 2.07 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,14 @@
 import java.util.*;
 
 class Solution {
-    
-    int count;
-    HashSet<Integer> set;
+
+    Set<Integer> check = new HashSet<>();
     
     public int solution(int[] nums) {
-        // 입력
-        int answer = 0;
-        count = nums.length / 2;
-        set = new HashSet<>();
-        
-        // set에 원소 넣기
         for(int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+            check.add(nums[i]);
         }
-        return Math.min(count, set.size());
+        
+        return Math.min(check.size(), nums.length / 2);
     }
 }
