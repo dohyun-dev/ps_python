e6bd392b8e76abb300c108973d9c9b5dd57bbd1b
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-07-24 17:33:18 +0900

    [Gold IV] Title: 작업, Time: 356 ms, Memory: 135656 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+n = int(input())
+indegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+dp = [0] * (n+1)
+t = [0]
+
+for i in range(1, n+1):
+    array = list(map(int, input().split()))
+    t.append(array[0])
+    if array[1] != 0:
+        for j in range(2, len(array)):
+            graph[array[j]].append(i)
+            indegree[i] += 1
+q = deque()
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        q.append(i)
+        dp[i] = t[i]
+while q:
+    now = q.popleft()
+    for i in graph[now]:
+        indegree[i] -= 1
+        dp[i] = max(dp[now] + t[i], dp[i])
+        if indegree[i] == 0:
+            q.append(i)
+print(max(dp))
