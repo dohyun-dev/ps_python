6d0b634a3de659899522647e376b24d85eacb83e
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-27 01:33:52 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,12 +1,16 @@
-def convert_stamp(date):
-    year, month, day = map(int, date.split("."))
-    return (year * 12 * 28) + (month * 28) + day
+def calc_date(date):
+    year, month, date = map(int, date.split("."))
+    return year * 28 * 12 + month * 28 + date
 
 def solution(today, terms, privacies):
-    terms_dict = {term[0]: int(term[2:]) * 28 for term in terms}
-    today = convert_stamp(today)
+    today = calc_date(today)
+    terms_dict = {t[0]: int(t[2:]) * 28 for t in terms}
     answer = []
-    for idx, privacy in enumerate(privacies, 1):
-        if today >= convert_stamp(privacy[:-2]) + terms_dict[privacy[-1]]:
-            answer.append(idx)
+    
+    for i, privacy in enumerate(privacies, 1):
+        date, kind = privacy.split()
+        pivacy_date = calc_date(date) + terms_dict[kind]
+        
+        if today >= pivacy_date:
+            answer.append(i)
     return answer
