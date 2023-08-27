92bbe72749ab1c62ed574afd81138ca4bd43e0fc
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-28 01:51:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,17 +1,10 @@
-from collections import Counter
-
 def solution(id_list, report, k):
     count = [[] for _ in range(len(id_list))]
-    check = set()
+    answer = [0] * len(id_list)
     
-    for data in report:
-        if data in check:
-            continue
-        check.add(data)
+    for data in set(report):
         user1, user2 = data.split()
         count[id_list.index(user2)].append(id_list.index(user1))
-    
-    answer = [0] * len(id_list)
     
     for i in range(len(id_list)):
         if len(count[i]) < k:
