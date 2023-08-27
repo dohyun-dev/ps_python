78e9a9e14934fe3343ebd0ecf5ddd209dffd8ba9
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-28 03:21:36 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,13 @@
+import re
+
+def solution(new_id):
+    answer = new_id.lower()
+    answer = re.sub("[^a-z0-9-_.]+", "", answer)
+    answer = re.sub("[.]{2,}", ".", answer)
+    answer = re.sub("^[.]|[.]$", "", answer).strip()
+    answer = answer if answer else "a"
+    answer = answer[:15]
+    answer = answer[:-1] if answer[-1] == '.' else answer
+    if len(answer) < 3:
+        answer += answer[-1] * (3 - len(answer))
+    return answer
