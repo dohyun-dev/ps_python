e1b5bee64c322b08e1600f473545be7eeb4196ad
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-28 02:09:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,25 +1,6 @@
-digit_dict = {
-    "zero": '0',
-    "one": '1',
-    "two": '2',
-    "three": '3',
-    "four": '4',
-    "five": '5',
-    "six": '6',
-    "seven": '7',
-    "eight": '8',
-    "nine": '9'
-}
+digit = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 def solution(s):
-    tmp = ""
-    answer = ""
-    for c in s:
-        if c.isdigit():
-            answer += c
-        else:
-            tmp += c 
-            if tmp in digit_dict:
-                answer += digit_dict[tmp]
-                tmp = ''
-    return int(answer)
+    for i in range(10):
+        s = s.replace(digit[i], str(i))
+    return int(s)
