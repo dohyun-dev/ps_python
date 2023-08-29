38ec31e82334c50f180b92cf31609ac1c40ef448
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-30 01:47:48 +0900

    [level 2] Title: [3차] 압축, Time: 0.31 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -0,0 +1,13 @@
+def solution(msg):
+    word_dict = {chr(ord('A') + i): i + 1 for i in range(26)}
+    answer = []
+    
+    w = ""
+    for c in msg:
+        w += c
+        if w not in word_dict:
+            answer.append(word_dict[w[:-1]])
+            word_dict[w] = len(word_dict) + 1
+            w = c
+    answer.append(word_dict[w])
+    return answer
