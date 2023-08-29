9600c150f3e2a6b6448423503bfa069bb58437eb
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2023-08-30 06:30:09 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.17 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -3,7 +3,6 @@ import re
 def solution(files):
     tmp = []
     for idx, file in enumerate(files):
-        m = re.match('([a-zA-Z\s.-]+)([\d]{1,5})([a-zA-Z0-9\s.-]*)', file)
-        tmp.append((idx, m.group(1), m.group(2), m.group(3)))
+        tmp.append((idx, *re.match('([a-zA-Z-. ]+)(\d{1,5})(.*)', file).groups()))
     tmp.sort(key=lambda x: (x[1].lower(), int(x[2]), idx))
     return ["".join(x[1:]) for x in tmp]
